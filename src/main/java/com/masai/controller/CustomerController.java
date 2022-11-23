0e8c1b242d7c4c458be0a7c48fba934aa83616d5
Vikram Chaurasia <chaurasiavikram23@gmail.com>
2022-11-23 17:54:07 +0530

    push

---
+++ src/main/java/com/masai/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package com.masai.controller;
+
+public class CustomerController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("heloo...");
+
+	}
+
+}
